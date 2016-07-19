b5595a004f16ba476f37e377a29a6f36dc4a5cef
Katerina Kampardi <kkampardi@gmail.com>
2016-07-19 20:23:42 +0300

    Add custom ungularjs directives and custom js product slider

--- shop/static/app/js/app.js
+++ shop/static/app/js/app.js
@@ -43,8 +43,59 @@ angular.module('statumeApp', ['ngRoute', 'ngAnimate' ])
 		var sliderFunc=function(){
 			timer=$timeout(function(){
 				scope.next();
-				timer=$timeout(sliderFunc,5000);
-			},5000);
+				timer=$timeout(sliderFunc,3000);
+			},3000);
+		};
+
+		sliderFunc();
+
+		scope.$on('$destroy',function(){
+			$timeout.cancel(timer);
+		});
+
+		/* End : For Automatic slideshow*/
+
+    },
+	//templateUrl:'views/templates/templateurl.html'
+  }
+})
+
+/*slider products*/
+.directive('productslider', function ($timeout) {
+  return {
+    restrict: 'AE',
+	replace: true,
+	scope:{
+		products: '='
+	},
+    link: function (scope, elem, attrs) {
+
+		scope.currentIndex=0;
+
+		scope.next=function(){
+			scope.currentIndex<scope.products.length-1?scope.currentIndex++:scope.currentIndex=0;
+		};
+
+		scope.prev=function(){
+			scope.currentIndex>0?scope.currentIndex--:scope.currentIndex=scope.products.length-1;
+		};
+
+		scope.$watch('currentIndex',function(){
+			scope.products.forEach(function(product){
+				product.visible=false;
+			});
+			scope.products[scope.currentIndex].visible=true;
+		});
+
+		/* Start: For Automatic slideshow*/
+
+		var timer;
+
+		var sliderFunc=function(){
+			timer=$timeout(function(){
+				scope.next();
+				timer=$timeout(sliderFunc,3000);
+			},3000);
 		};
 
 		sliderFunc();
--- shop/static/app/js/controllers.js
+++ shop/static/app/js/controllers.js
@@ -10,5 +10,11 @@ angular.module('statumeApp')
         {src:'cup4.jpg',title:'Pic 4'},
         {src:'img5.png',title:'Pic 5'}];
 
+        $scope.products=[
+          {title:'Demo Product One', content:'Assault voodoo god paranoid tattoo modem kanji drone urban hotdog uplink computer. ', price: '55'},
+          {title:'Demo Product Two', content:'Artisanal marketing RAF into advert geodesic sprawl realism woman dome footage courier table drone media youtube ablative', price: '52'},
+          {title:'Demo Product Three', content:'Dome car papier-mache geodesic wonton soup RAF warehouse woman military-grade numinous shrine nodality pre. Physical drone chrome sunglasses footage vinyl disposable office denim youtube. Systemic-ware advert geodesic alcohol assassin monofilament shrine Chiba voodoo god film convenience store disposable industrial grade camera cartel.', price: '38'},
+          {title:'Demo Product Four', content:'Assault voodoo god paranoid tattoo modem kanji drone urban hotdog uplink computer. ', price: '20'}
+        ]
   }])
 ;
